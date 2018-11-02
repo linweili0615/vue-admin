226201e74dcd5bbd74f6c281619b6138c927a249
linweili <295287765@qq.com>
2018-11-02 18:03:04 +0800

    修改mock服务器

--- src/api/login.js
+++ src/api/login.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function login(username, password) {
   return request({
-    url: '/user/login',
+    url: '/user/2login',
     method: 'post',
     data: {
       username,
