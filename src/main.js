dd8acba2fd7062b9dc79c459b533af4b4290b4d3
linweili <295287765@qq.com>
2018-11-08 16:47:55 +0800

    axios 全局

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import '@/styles/index.scss' // global css
 import App from './App'
 import router from './router'
 import store from './store'
-import service from './utils/request'
+import service from '@/utils/request'
 
 import '@/icons' // icon
 import '@/permission' // permission control
