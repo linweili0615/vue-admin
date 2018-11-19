2ff2e8a11ed7ffa77a89f6f4d140259c776da136
linweili <295287765@qq.com>
2018-11-19 17:10:20 +0800

    删除locale，使用默认中文

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 import '@/styles/index.scss' // global css
 import App from './App'
 import router from './router'
@@ -13,7 +12,7 @@ import '@/icons' // icon
 import '@/permission' // permission control
 
 
-Vue.use(ElementUI, { locale })
+Vue.use(ElementUI)
 
 
 import { Message } from 'element-ui'
