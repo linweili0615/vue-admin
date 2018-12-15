5ae9bd7c59681ad95c6d8cd1f9a19f8f015c604e
linweili <295287765@qq.com>
2018-12-15 16:58:13 +0800

    case

--- src/views/test/TaskCase.vue
+++ src/views/test/TaskCase.vue
@@ -8,7 +8,8 @@
 
             <div slot="header" class="clearfix">
               <span>用例信息</span>
-              <el-button style="float: right; padding: 5px;margin-left: 3px" type="success">
+              <el-button style="float: right; padding: 5px;margin-left: 3px"
+                         type="success" @click="changeStatus">
                 切换结果
                 <i class="el-icon-sort"></i>
               </el-button>
@@ -22,9 +23,8 @@
               <div style="z-index: 9;float: right;position: relative;">
                 <el-button  type="primary" size="medium">执行测试</el-button>
               </div>
-              <el-tabs v-model="activeName2" type="card" @tab-click="handleClick">
-                <el-tab-pane label="步骤" name="third">
-
+              <el-tabs v-model="activeName1" type="card" @tab-click="handleClick">
+                <el-tab-pane label="步骤" name="step">
                     <el-checkbox-group class="checkbox-group" v-model="task.list">
                       <el-scrollbar wrap-class="scrollbar-wrap" class="el-scrollbar-wrap">
                       <div class="checkbox-item">
@@ -39,11 +39,8 @@
                       </div>
                       </el-scrollbar>
                     </el-checkbox-group>
-
-
-
                 </el-tab-pane>
-                <el-tab-pane label="文档" name="fourth">文档</el-tab-pane>
+                <el-tab-pane label="文档" name="api">文档</el-tab-pane>
               </el-tabs>
             </template>
 
@@ -52,18 +49,17 @@
 
         </div>
       </el-col>
-      <el-col :span="14">
+
+      <el-col :span="14" v-show="activeStatus">
         <div class="grid-content bg-purple">
           <el-card class="box-card">
             <div slot="header" class="clearfix">
               <span>案例列表</span>
             </div>
-
             <template>
               <el-tabs v-model="activeName2" type="card" @tab-click="handleClick">
-
-                <el-tab-pane label="接口列表" name="third">
-                  <el-form :inline="true" :model="search" class="demo-form-inline" size="medium">
+                <el-tab-pane label="接口列表" name="interface">
+                  <el-form :inline="true" :model="search" class="demo-form-inline" size="mini">
                     <el-form-item label="接口名称：">
                       <el-input v-model="search.name" placeholder="请输入接口名称"></el-input>
                     </el-form-item>
@@ -87,26 +83,24 @@
                   <template>
                     <el-table
                       ref="multipleTable"
+                      @row-click="rowClick"
                       :data="apilist"
                       height="600"
                       size="medium"
                       tooltip-effect="dark"
+                      highlight-current-row
+                      empty-text="暂无数据"
                       style="width: 100%"
                       @selection-change="handleSelectionChange">
                       <el-table-column
                         type="selection"
                         width="30">
                       </el-table-column>
+
                       <el-table-column
                         prop="name"
                         label="接口名称"
                           width="250">
-                      </el-table-column>
-                      <el-table-column
-                        prop="url"
-                        label="请求地址"
-                        width="550"
-                        show-overflow-tooltip>
                       </el-table-column>
                       <el-table-column  label="请求方式" width="85" show-overflow-tooltip>
                         <template slot-scope="scope">
@@ -114,18 +108,64 @@
                           <el-tag v-show="scope.row.method === 'GET'">{{ scope.row.method }}</el-tag>
                         </template>
                       </el-table-column>
+                      <el-table-column
+                        prop="url"
+                        label="请求地址"
+                        width="450"
+                        show-overflow-tooltip>
+                      </el-table-column>
+                      <el-table-column prop="paramstype" label="参数类型" width="85" show-overflow-tooltip>
+                      </el-table-column>
+
+
                     </el-table>
                   </template>
 
                 </el-tab-pane>
-
-                <el-tab-pane label="定时任务补偿" name="fourth">文档</el-tab-pane>
+                <!--<el-tab-pane label="测试结果" name="result">文档</el-tab-pane>-->
               </el-tabs>
             </template>
 
           </el-card>
         </div>
       </el-col>
+
+      <el-col :span="14" v-show="!activeStatus">
+        <div class="grid-content bg-purple">
+          <el-card class="box-card">
+            <div slot="header" class="clearfix">
+              <span>执行结果</span>
+            </div>
+
+                  <el-collapse v-model="activeResult" @change="handleChange">
+                    <el-collapse-item name="1">
+                      <template slot="title">
+                        测试接口一<i class="el-icon-success" style="color: greenyellow"></i>
+                      </template>
+                      <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+                      <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="测试接口二" name="2">
+                      <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+                      <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="测试接口三" name="3">
+                      <div>简化流程：设计简洁直观的操作流程；</div>
+                      <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
+                      <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+                    </el-collapse-item>
+                    <el-collapse-item title="测试接口四" name="4">
+                      <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+                      <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+                    </el-collapse-item>
+                  </el-collapse>
+
+
+          </el-card>
+        </div>
+      </el-col>
+
+
     </el-row>
 
   </div>
@@ -137,7 +177,10 @@
     data() {
       return {
         options: [],
-        activeName2: 'third',
+        activeName1: 'step',
+        activeName2: 'interface',
+        activeResult: ['1'],
+        activeStatus: true,
         api: {
           list: []
         },
@@ -159,6 +202,9 @@
       onSubmit() {
         console.log('submit!');
       },
+      changeStatus(){
+        this.activeStatus = !this.activeStatus
+      },
       toggleSelection(rows) {
         if (rows) {
           rows.forEach(row => {
@@ -170,6 +216,11 @@
       },
       handleSelectionChange(val) {
         this.api.list = val;
+      },
+      rowClick(row,event,column){
+        if(event.target.nodeName!="INPUT"){
+          this.$refs.multipleTable.toggleRowSelection(row);
+        }
       },
       getTaskList(){
         this.$axios.post('/task/extend/info','81598efb-ffa9-11e8-a19c-0242ac110002')
@@ -211,6 +262,9 @@
             this.options = [];
             this.$message.error("获取测试集失败")
           })
+      },
+      handleChange(val) {
+        console.log(val);
       },
       handleOptionsChange(val){
         // this.project_id = val[0]
@@ -292,7 +346,7 @@ ul li{
     margin-left: 0;
   }
   /deep/ .el-checkbox__label{
-    font-size: 13px;
+    font-size: 14px;
     width: 100%;
   }
 
