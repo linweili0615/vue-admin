1946d5ea70ce9acc2802ab69cc9e15fa295fed98
linweili <295287765@qq.com>
2018-12-20 23:17:46 +0800

    返回结果

--- src/views/test/TaskCase.vue
+++ src/views/test/TaskCase.vue
@@ -161,26 +161,17 @@
               <span>执行结果</span>
             </div>
 
-                  <el-collapse v-model="activeResult" @change="handleChange">
-                    <el-collapse-item name="1">
-                      <template slot="title">
-                        测试接口一<i class="el-icon-success" style="color: greenyellow"></i>
-                      </template>
-                      <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
-                      <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
-                    </el-collapse-item>
-                    <el-collapse-item title="测试接口二" name="2">
-                      <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
-                      <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
-                    </el-collapse-item>
-                    <el-collapse-item title="测试接口三" name="3">
-                      <div>简化流程：设计简洁直观的操作流程；</div>
-                      <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
-                      <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
-                    </el-collapse-item>
-                    <el-collapse-item title="测试接口四" name="4">
-                      <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
-                      <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+                  <el-collapse :data="result_list" v-for="(item,index) in result_list">
+                    <el-collapse-item :name="index" :title="item.api_name" :key="item.api_id">
+                      <pre>method:  {{item.req_method}}</pre>
+                      <pre>url:<br/>{{item.req_url}}</pre>
+                      <pre>params:<br/>{{item.req_body}}</pre>
+                      <pre>request_cookies:<br/>{{item.req_cookies}}</pre>
+                      <pre>request_headers:<br/>{{item.req_headers}}</pre>
+                      <pre>response_code: {{item.res_code}}</pre>
+                      <pre>response_cookies:<br/>{{item.res_cookies}}</pre>
+                      <pre>response_headers:<br/>{{item.res_headers}}</pre>
+                      <pre>response_body:<br/>{{item.res_body}}</pre>
                     </el-collapse-item>
                   </el-collapse>
 
@@ -237,7 +228,14 @@
         apilist: [],
         tasklist: [],
         deal_list:[],
-        deal_status: 0,
+        result_list: [
+          {
+            code: 502,
+            content: '返回内容',
+            cookies: '返回cookies',
+            headers: '返回headers'
+          }
+        ],
         dialogFormVisible: false,
         draw:{
           name: '',
@@ -370,11 +368,13 @@
         this.$axios.post('/task/test', '81598efb-ffa9-11e8-a19c-0242ac110002')
           .then(response => {
             if(response.data.status === 'success'){
-              console.log(response.data.data)
+              this.result_list = response.data.data
+              this.activeStatus = !this.activeStatus
             }
           })
           .catch(error => {
               console.log(error)
+            this.$message.error('执行异常')
           })
       },
       deleteTask(){
@@ -591,7 +591,11 @@ ul li{
 .el-form-item {
   margin-bottom: 10px;
 }
-
+pre {
+  overflow-x: auto;
+  white-space: pre-wrap;
+  word-wrap: break-word;
+}
 
 </style>
 
